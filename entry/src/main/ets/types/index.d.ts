deebc7b1d6d7f384f3f2b3e062105d2957b69ad1
Oreki <2763981847@qq.com>
2023-11-15 19:13:29 +0800

    为代码添加了详细的注释

--- entry/src/main/ets/types/index.d.ts
+++ entry/src/main/ets/types/index.d.ts
@@ -1,10 +1,12 @@
+// 定义接口 DateRange，表示日期范围，包括开始日期和结束日期
 export interface DateRange {
-  beginDate: number;
-  endDate: number;
+  beginDate: number; // 开始日期的时间戳
+  endDate: number;   // 结束日期的时间戳
 }
 
+// 定义接口 AccountTable，表示数据库中的账户表结构
 export interface AccountTable {
-  tableName: string;
-  sqlCreate: string;
-  columns: string[];
+  tableName: string; // 表名
+  sqlCreate: string; // 创建表的 SQL 语句
+  columns: string[]; // 表的列名数组
 }
