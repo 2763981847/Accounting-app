cca509fdf41ec87ae3921deb9e2e7fb2c38ac03f
Oreki <2763981847@qq.com>
2023-11-15 08:42:21 +0800

    优化了项目结构

---
+++ entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/common/beans/ClassifiedStatistic.js
@@ -0,0 +1,13 @@
+export default class ClassifiedStatistic {
+    constructor(typeText, accountType, amount = 0, count = 0) {
+        this.typeText = '';
+        this.accountType = 0;
+        this.amount = 0;
+        this.count = 0;
+        this.typeText = typeText;
+        this.accountType = accountType;
+        this.amount = amount;
+        this.count = count;
+    }
+}
+//# sourceMappingURL=ClassifiedStatistic.js.map
